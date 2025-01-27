057c283d24a68db448c38342fe4a23e4d57c2cd7
Mia West <mia.west@durham.ac.uk>
2025-01-27 10:49:29 -0500

    Adding documentation for DifferencePlot.py

--- DifferencePlot.py
+++ DifferencePlot.py
@@ -5,9 +5,26 @@ from scipy import interpolate, optimize
 import csv
 from multiprocessing import Pool
 
+'''
+    Identical function operation to "differencePlot" in Plotter.py. Just in a separate file allowing for more control over plotting parameters.
+
+        "plotDifference" allows you to see, for the Csaki (half-transparency) and normal (full-transparency) cases, 
+        the difference in GW data for cases with the same zero-temp masses. These are indicated by having the same marker. 
+        However, this function can only handle up to 22 data points or it does not have enough markers.
+                
+        TLDR CSAKI IS HALF TRANSPARENCY; NORMAL IS FULL TRANSPARENCY. CAN ONLY HANDLE 22 DATA POINTS.
+                
+        plotDifference:
+            INPUTS: (resC, resN)
+                (np.array, np.array)
+
+'''
+
+
 plt.rcParams["font.family"] = "serif"
 plt.rcParams["mathtext.fontset"] = "cm"
 plt.rcParams["font.size"]= 11
+
 
 
 def plotDifference(resC, resN):
@@ -15,18 +32,24 @@ def plotDifference(resC, resN):
     markers = [".","o","v","^","<",">","1","2","3","4","8","s","p","P","*","h","H","+","x","D","d","|"]
 	
     colormap = plt.cm.viridis #or any other colormap
-    normalize = matplotlib.colors.Normalize(vmin=3, vmax=20)
+    normalize = matplotlib.colors.Normalize(vmin=3, vmax=20) #Set manually this time.
 	#For the colour map
     fig= plt.subplot()
 
     for i in range(len(resC)):
         if resC[i,2]!=0 and resN[i,2]!=0:
-            plt.scatter(resC[i,2], resC[i,1], c = resC[i,3], alpha=1/(3), marker=markers[-1], cmap=colormap, norm=normalize)
+            #Csaki term has half-transparency.
+            plt.scatter(resC[i,2], resC[i,1], c = resC[i,3], alpha=1/(2), marker=markers[-1], cmap=colormap, norm=normalize)
+            #Normal term has full-transparency.
             plt.scatter(resN[i,2], resN[i,1], c = resN[i,3], alpha=1/(1), marker=markers[-1], cmap=colormap, norm=normalize)
+            #Pops off the last marker to move onto the next one.
             markers.pop()
 
     fig.set_xscale("log")
     fig.set_yscale("log")
+    
+    ##Set the following plotting parameters as required:
+    
     #fig.set_ylim([0.01484,0.01532])
     #fig.set_yticks([], minor=True)
     #fig.set_xticks([], minor=True)
@@ -35,7 +58,7 @@ def plotDifference(resC, resN):
     #fig.set_yticks([1.49E-2,1.5E-2,1.51E-2,1.52E-2,1.53E-2])
     #fig.set_yticklabels([1.49E-2,1.5E-2,1.51E-2,1.52E-2,1.53E-2])
 
-    plt.colorbar(label=r"Mass Ratio $m_\sigma/m_{\eta'}$")
+    plt.colorbar(label=r"$m^2_\sigma$")
     fig.set_xlabel(r'$\beta/H$',fontsize=13)
     fig.set_ylabel(r'$\alpha$',fontsize=15)
     plt.tight_layout()
@@ -44,7 +67,7 @@ def plotDifference(resC, resN):
     #plt.savefig('DifferencePlotN3F6.pdf',bbox_inches="tight")
 
 	
-
+#Load up the data.
 resN = np.genfromtxt(f'Test_N3F6_Normal.csv', delimiter=',', dtype=float, skip_header=1)[:,4:]
 resC = np.genfromtxt(f'Test_N3F6_Csaki.csv', delimiter=',', dtype=float, skip_header=1)[:,4:]
 
